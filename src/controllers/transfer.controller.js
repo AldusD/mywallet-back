01513ff75c8fe91cc96cd65882dbd455a2dd0bf2
aldusd <aldusdanielfsm@gmail.com>
2023-03-15 17:14:43 -0300

    feat: creates logout route

--- src/controllers/transfer.controller.js
+++ src/controllers/transfer.controller.js
@@ -34,4 +34,36 @@ const addTransfer = async (req, res) => {
     }
 }
 
+export const deleteTransfer = async (req, res) => {
+    const transferId = req.body.transferId;
+    const userId = res.locals.id;
+
+    try {
+        const transfer = await db.collection("transfers").findOne({ id: transferId });
+        if(transfer.userId != userId) return res.sendStatus(401);
+        const transferDelete = await db.collection("transfer").deleteOne({ id: transferId });
+        return res.sendStatus(204);
+    } catch (error) {
+        console.error(error);
+        return res.sendStatus(500);
+    }
+}
+
+export const putTransfer = async (req, res) => {
+    console.log('deltr');
+    const transferId = req.body.transferId;
+    const { value, type, description } = res.locals.transfer;
+
+    try {
+        const transfer = await db.collection("transfers").findOne({ id: transferId });
+        console.log('todelt', transfer)
+        if(transfer.userId != userId) return res.sendStatus(401);
+        const transferUpdate = await db.collection("transfer").updateOne({ id: transferId }, { value, type, description });
+        return res.sendStatus(200);
+    } catch (error) {
+        console.error(error);
+        return res.sendStatus(500);
+    }
+}
+
 export { getTransfers, addTransfer };
